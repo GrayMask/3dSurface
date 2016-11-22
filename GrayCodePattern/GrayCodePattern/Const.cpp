5c86758d44dbbfe1aa816af9f983efd737bc9c72
GrayMask <407247898@qq.com>
2016-11-22 14:01:40 +0900

    change gitignore

---
+++ GrayCodePattern/GrayCodePattern/Const.cpp
@@ -0,0 +1,11 @@
+#include "Const.h";
+#include <opencv2/core.hpp>;
+using namespace cv;
+extern const String images_dir = "D:\\document\\project\\3dSurface\\code\\partten_images4\\";
+extern const String imagesName_file = images_dir + "imgName.yaml";
+extern const String calib_file = "D:\\document\\project\\3dSurface\\code\\calibration_result\\camera2.xml";
+extern const int proj_width = 1024;
+extern const int proj_height = 768;
+
+extern const size_t white_thresh = 1;
+extern const size_t black_thresh = 1;
